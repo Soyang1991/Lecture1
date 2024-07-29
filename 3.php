3fe20ff6b79da183a7a4c34533c46ae9a73460af
sagar3562 <sagarmahato356@gmail.com>
2024-07-29 19:51:26 +1000

    ghjgjhg

---
+++ 3.php
@@ -0,0 +1,46 @@
+<?php
+$var1 = 3;
+$var2 = 2;
+
+/* if example */
+if ($var1 == $var2) {
+  echo("$var1 is the same as $var2 <br>");
+} else {
+  echo("$var1 is NOT the same as $var2 <br>");
+}
+
+/* if-else example with greater than operator */
+if ($var1 > $var2) {
+  echo("$var1 is greater than $var2 <br>");
+} else {
+  echo("$var1 is NOT greater than $var2 <br>");
+}
+
+/* if-else example with less than operator */
+if ($var1 < $var2) {
+  echo("$var1 is less than $var2 <br>");
+} else {
+  echo("$var1 is NOT less than $var2 <br>");
+}
+
+/* if-else example with greater or equal to operator */
+if ($var1 >= $var2) {
+  echo("$var1 is greater than or equal to $var2 <br>");
+} else {
+  echo("$var1 is less than $var2 <br>");
+}
+
+/* if-else example with less or equal to operator */
+if ($var1 <= $var2) {
+  echo("$var1 is less than or equal to $var2 <br>");
+} else {
+  echo("$var1 is greater than $var2 <br>");
+}
+
+/* if-else example with not equal operator */
+if ($var1 != $var2) {
+  echo("$var1 is NOT equal to $var2 <br>");
+} else {
+  echo("$var1 is equal to $var2 <br>");
+}
+?>
